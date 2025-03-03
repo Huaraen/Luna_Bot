f28c8ac5afb1f54c00fe0b3a91c4d252aa769d30
Huara <Huarahj@hotmail.com>
2025-03-03 10:36:42 -0300

    tratamento de erros

--- switch.py
+++ switch.py
@@ -2,8 +2,8 @@ from microfone_off import Console
 from microfone import Mic
 from power import Power_on, Power_off
 from commands import Server
-
 server = Server()
+
 class Switch:
     """Classe responsável por ligar e desligar o sistema."""
     def __init__(self, bot_name):
@@ -15,18 +15,47 @@ class Switch:
         self.power = True
 
     def check_power(self):
-        self.estado_atual.check_power(self)
+        try:
+            self.estado_atual.check_power(self)
+        except Exception as e:
+            print(f"Erro ao verificar o estado de energia: {e}")
 
     def ligar(self):
-        self.estado_atual.ligar(self)
+        try:
+            self.estado_atual.ligar(self)
+        except Exception as e:
+            print (f"Erro ao ligar: {e}")
 
     def desligar(self):
-        self.estado_atual.desligar(self)
+        try:
+            self.estado_atual.desligar(self)
+        except Exception as e:
+            print (f"Erro ao desligar:{e}")
 
     def abrir_console(self):
-        self.executavel = self.console
-        server.notification("Iniciando comando pelo teclado. .")
+        try:
+            self.executavel = self.console
+            server.notification("Iniciando comando pelo teclado. .")
+        except Exception as e:
+            print (f"Erro ao abrir console: {e}")
 
     def fechar_console(self):
-        self.executavel = self.mic
-        server.notification("Iniciando comando por voz. .")
+        try:
+            self.executavel = self.mic
+            server.notification("Iniciando comando por voz. .")
+        except Exception as e:
+            print (f"Erro ao fechar console: {e}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
